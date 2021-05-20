4887ef01ba2059573efd6e885cbddff88c22df42
MaximK <57616894+MaximKirpichev@users.noreply.github.com>
2021-05-20 21:59:59 +0300

    Решение проблемы с merge

--- Project Madness/Assets/Scripts/RoomPlacer.cs
+++ Project Madness/Assets/Scripts/RoomPlacer.cs
@@ -5,39 +5,22 @@ using UnityEngine;
 
 public class RoomPlacer : MonoBehaviour
 {
-    public Room[] RoomPrefabs;
-    public Room StartingRoom;
-    public GameObject player; 
+    public GameObject[] RoomPrefabs;
+    public GameObject StartingRoom;
 
-    private Room[,] spawnedRooms;
+    private GameObject[,] spawnedRooms;
     private void Start()
     {
-        spawnedRooms = new Room[11, 11];
+        spawnedRooms = new GameObject[11, 11];
         spawnedRooms[5, 5] = StartingRoom;
 
         for (int i = 0; i < 12; i++)
         {
             PlaceOneRoom();
         }
-
-        if (spawnedRooms[6, 5] != null && !spawnedRooms[6, 5].DoorL.activeInHierarchy)
-        {
-            StartingRoom.DoorR.SetActive(false);
-        }
-        if (spawnedRooms[4, 5] != null && !spawnedRooms[4, 5].DoorR.activeInHierarchy)
-        {
-            StartingRoom.DoorL.SetActive(false);
-        }
-        if (spawnedRooms[5, 6] != null && !spawnedRooms[5, 6].DoorD.activeInHierarchy)
-        {
-            StartingRoom.DoorU.SetActive(false);
-        }
-        if (spawnedRooms[5, 4] != null && !spawnedRooms[5, 4].DoorU.activeInHierarchy)
-        {
-            StartingRoom.DoorD.SetActive(false);
-        }
     }
 
+    // Update is called once per frame
     private void PlaceOneRoom()
     {
         HashSet<Vector2Int> vacantPlaces = new HashSet<Vector2Int>();
@@ -58,59 +41,10 @@ public class RoomPlacer : MonoBehaviour
             }
         }
 
-        Room newRoom = Instantiate(RoomPrefabs[Random.Range(0, RoomPrefabs.Length)]);
-        int limit = 500;
-        while (limit-- > 0)
-        {
-            Vector2Int position = vacantPlaces.ElementAt(Random.Range(0, vacantPlaces.Count));
+        GameObject newRoom = Instantiate(RoomPrefabs[Random.Range(0, RoomPrefabs.Length)]);
+        Vector2Int position = vacantPlaces.ElementAt(Random.Range(0, vacantPlaces.Count));
+        newRoom.transform.position = new Vector2(position.x, position.y) * 11;
 
-            if (ConnectToSomething(newRoom, position))
-            {
-                newRoom.transform.position = new Vector2(position.x, position.y) * 11;
-                spawnedRooms[position.x, position.y] = newRoom;
-                return;
-            }
-        }
-    }
-
-    private bool ConnectToSomething(Room room, Vector2Int p)
-    {
-        int maxX = spawnedRooms.GetLength(0) - 1;
-        int maxY = spawnedRooms.GetLength(1) - 1;
-
-        List<Vector2Int> neighbours = new List<Vector2Int>();
-
-        if (room.DoorU != null && p.y < maxY && spawnedRooms[p.x, p.y + 1]?.DoorD != null) neighbours.Add(Vector2Int.up);
-        if (room.DoorD != null && p.y > 0 && spawnedRooms[p.x, p.y - 1]?.DoorU != null) neighbours.Add(Vector2Int.down);
-        if (room.DoorR != null && p.x < maxX && spawnedRooms[p.x + 1, p.y]?.DoorL != null) neighbours.Add(Vector2Int.right);
-        if (room.DoorL != null && p.x > 0 && spawnedRooms[p.x - 1, p.y]?.DoorR != null) neighbours.Add(Vector2Int.left);
-
-        if (neighbours.Count == 0) return false;
-
-        Vector2Int selectedDirection = neighbours[Random.Range(0, neighbours.Count)];
-        Room selectedRoom = spawnedRooms[p.x + selectedDirection.x, p.y + selectedDirection.y];
-
-        if (selectedDirection == Vector2Int.up)
-        {
-            room.DoorU.SetActive(false);
-            selectedRoom.DoorD.SetActive(false);
-        }
-        else if (selectedDirection == Vector2Int.down)
-        {
-            room.DoorD.SetActive(false);
-            selectedRoom.DoorU.SetActive(false);
-        }
-        else if (selectedDirection == Vector2Int.right)
-        {
-            room.DoorR.SetActive(false);
-            selectedRoom.DoorL.SetActive(false);
-        }
-        else if (selectedDirection == Vector2Int.left)
-        {
-            room.DoorL.SetActive(false);
-            selectedRoom.DoorR.SetActive(false);
-        }
-
-        return true;
+        spawnedRooms[position.x, position.y] = newRoom;
     }
 }
