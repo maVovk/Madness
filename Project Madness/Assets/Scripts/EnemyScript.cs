d92847b082b9d2de2a8dfeb9278bbf9898f1cfe3
MaximK <57616894+MaximKirpichev@users.noreply.github.com>
2021-05-24 14:31:42 +0300

    Полу работаютщий скрипт врагов

--- Project Madness/Assets/Scripts/EnemyScript.cs
+++ Project Madness/Assets/Scripts/EnemyScript.cs
@@ -1,31 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class EnemyScript : MonoBehaviour
 {
     public Camera mainCamera;
-    public float speed = 10f; // скорость оружия
+    public float speed = 10f;
     public float damage = 20f; // дамаг
     public float defence = 0.5f; // защита
-    public Rigidbody2D rb; // ригидбоди, который отвечает за нахождение врагов в радиусе видимости
+    Rigidbody2D rb; // ригидбоди, который отвечает за нахождение врагов в радиусе видимости
     public GameObject enemy;
-    public GameObject player; // герой
+    public Transform player; // герой
     public float hp = 100f; // HP
-    public float strenght = 0.5f; // def
+    public float strength = 0.5f; // def
     Vector2 enemyVec; // вектор направления движения
     Vector2 dash; // вектор направления рывка
     float angle; // угол поворота героя
-    
-    void Update()
+    public int dist;
+    Transform target;
+    Transform spawnPos;
+    bool goHome = false;
+    private void Start()
     {
-        if (hp <= 0)
-            Destroy(enemy);
+        spawnPos = this.transform;
+        target = GameObject.FindWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
-	{
-        
-	}
+    void Update()
+    {
+        if (goHome)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, spawnPos.position, speed * Time.deltaTime);
+        }
+        CheckDistance();
+        if (hp <= 0)
+            Destroy(enemy);
+    }  
+    
+    private void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= dist)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        goHome = true;
+    }
 }
 
