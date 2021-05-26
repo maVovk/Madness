8e3a5ea43cab2592404e488d364fa7ef415cad1a
maVovk <MichaelVovk@yandex.ru>
2021-05-26 11:18:27 +0300

    Добавлены работающий рывок и немного изменена система передвижения

--- Project Madness/Assets/Scripts/MovementScript.cs
+++ Project Madness/Assets/Scripts/MovementScript.cs
@@ -14,10 +14,11 @@ public class MovementScript : MonoBehaviour
 
     public Rigidbody2D rb; // ригидбоди, который отвечает за перемещение игрока
     
-    Vector2 movement; // вектор направления движения
-    Vector2 dash; // вектор направления рывка
-    
-    public float dashMultiplier = 0.2f; // множитель расстояния рывка
+    Vector3 movement; // вектор направления движения
+    bool isDashCooldown = false;
+    float dashCooldown = 5f; // время отката рывка
+
+    public float dashMultiplier = 100f; // множитель расстояния рывка
     public float angle; // угол поворота героя
 
     public GameObject playerBody; // спрайт на герое
@@ -36,20 +37,32 @@ public class MovementScript : MonoBehaviour
     {
         // управление на WASD и стрелки
         // определяем вектор, на который будем перемещаться
-        movement.x = Input.GetAxis("Horizontal");
-        movement.y = Input.GetAxis("Vertical");
+        float x = 0f, y = 0f;
+		if (Input.GetAxis("Horizontal") > 0)
+		{
+            x = 1f;
+		} else if(Input.GetAxis("Horizontal") < 0)
+		{
+            x = -1f;
+		}
 
-        // рывок делается по направлению мыши
-        // определяем вектор, на который будем делать рывок
-        dash = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        dash.x -= rb.position.x;
-        dash.y -= rb.position.y;
+		if (Input.GetAxis("Vertical") > 0)
+		{
+            y = 1f;
+		} else if(Input.GetAxis("Vertical") < 0)
+		{
+            y = -1f;
+		}
+
+        movement = new Vector3(x, y).normalized;
     }
 
     void FixedUpdate()
     {
         // движение игрока
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        //rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        rb.velocity = movement * speed;
+
         if(movement.x != 0 || movement.y != 0)
         {
             if (!step.isPlaying)
@@ -61,12 +74,14 @@ public class MovementScript : MonoBehaviour
         else step.Stop();
 
         // рывок игрока
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashCooldown)
         {
-            rb.MovePosition(new Vector2(rb.position.x + dash.x * dashMultiplier, rb.position.y + dash.y * dashMultiplier));
+            isDashCooldown = true;
+            rb.MovePosition(transform.position + movement * dashMultiplier); // рывок делается по направлению движения игрока
+            StartCoroutine(DashCooldown());
         }
 
-        playerBody.transform.position = rb.position;
+        //playerBody.transform.position = rb.position;
 		//здесь ещё нужно сменить анимацию, но я не знаю как это делается
 
         // смена оружия
@@ -84,4 +99,10 @@ public class MovementScript : MonoBehaviour
         //Debug.Log(index);
         gunObjects[index].transform.position = new Vector3(rb.transform.position.x + 0.25f, rb.transform.position.y + 0.2f, rb.transform.position.z);
     }
+
+    IEnumerator DashCooldown()
+	{
+        yield return new WaitForSeconds(dashCooldown);
+        isDashCooldown = false;
+	}
 }
